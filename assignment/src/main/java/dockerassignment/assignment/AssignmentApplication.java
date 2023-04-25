49478e5e2679229f15e10a8d39531c03906b2a18
Rakesh1208 <52816767+Rakesh1208@users.noreply.github.com>
2023-04-25 15:39:37 +0530

    Docker

---
+++ assignment/src/main/java/dockerassignment/assignment/AssignmentApplication.java
@@ -0,0 +1,14 @@
+package dockerassignment.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentApplication {
+
+	public static void main(String[] args) {
+		  System.out.println("Hello dinesh World!"); 
+		SpringApplication.run(AssignmentApplication.class, args);
+	}
+
+}
